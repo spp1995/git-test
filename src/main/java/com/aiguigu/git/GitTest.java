57bdf51c8978a0dcc567bbf84a096572114ad62f
spp1995 <36719226+spp1995@users.noreply.github.com>
2022-05-20 14:22:39 +0800

    pull commit

--- src/main/java/com/aiguigu/git/GitTest.java
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("push  SSH test!");
         System.out.println("push  SSH test2!");
+        System.out.println("pull  test!");
     }
 }
